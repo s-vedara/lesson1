efb0b2632e79a97277b9f63a1785c9b2a47fcf78
Ivanov Vladimir <v_ivanov@almaty.gamma.kz>
2019-07-11 16:34:58 +0300

    Version1.0

---
+++ test.py
@@ -0,0 +1,15 @@
+
+
+
+print("Hello world!")
+
+print(3 + 4)
+print(3 * 5)
+print(3 ** 2)
+
+name = input("Как Вас зовут? ")
+print("Привет,", name)
+
+
+# Команды можно разделять точкой запятой.
+a = 1; b = 2; print(a, b)
